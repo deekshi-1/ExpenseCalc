8af74818ec9ff547c4bbac2d6c29a52f030f2448
deekshi-1 <deekshithc1@gmail.com>
2025-11-20 21:00:55 +0530

    ENVIRONMENT

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // apiUserUrl: 'http://localhost:2001/',
-    apiUserUrl: 'https://expensecalc-backend.onrender.com',
+    apiUserUrl: 'https://expensecalc-backend.onrender.com/',
 };
